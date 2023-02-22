11697135e246b38c7cec5ceb049f30475ef9246e
Letícia Escobar <leticiaescobaraujo@gmail.com>
2023-02-22 17:37:14 -0300

    arquivos atualizados

--- src/main/java/br/com/ifms/lp2/javabeans/Aluno.java
+++ src/main/java/br/com/ifms/lp2/javabeans/Aluno.java
@@ -1,9 +1,10 @@
 package br.com.ifms.lp2.javabeans;
 
+import org.springframework.data.annotation.Id;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
